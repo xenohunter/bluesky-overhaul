2918dea89adffcf0efaf0d4c39826b02cae9a2e8
xenohunter <xenohunter@protonmail.ch>
2023-04-14 17:00:42 +0200

    Refactor pipeline launching mechanism

--- src/content.js
+++ src/content.js
@@ -1,11 +1,11 @@
 import '@webcomponents/custom-elements';
-import {getModalContainer, getComposePostModal} from './utils/elementsFinder';
+import {getRootContainer, getModalContainer, getComposePostModal} from './utils/elementsFinder';
 import {PostModalPipeline} from './pipelines/postModal';
 import {EmojiPipeline} from './pipelines/emoji';
 import {QuotePostPipeline} from './pipelines/quotePost';
 
 setTimeout(() => {
-  const modalContainer = getModalContainer();
+  const modalContainer = getModalContainer(getRootContainer());
 
   const postModalPipeline = new PostModalPipeline();
   const pauseExitModal = postModalPipeline.pauseExit.bind(postModalPipeline);
@@ -14,20 +14,24 @@ setTimeout(() => {
   const emojiPipeline = new EmojiPipeline(pauseExitModal, resumeExitModal);
   const quotePostPipeline = new QuotePostPipeline();
 
-  const composePostPipelines = [postModalPipeline, emojiPipeline, quotePostPipeline];
+  const pipelines = {
+    compose: [postModalPipeline, emojiPipeline, quotePostPipeline]
+  };
+
+  const deployPipelines = (type, target = {}) => pipelines[type].forEach(pipeline => pipeline.deploy(target, type));
+  const terminatePipelines = (type) => pipelines[type].forEach(pipeline => pipeline.terminate());
+  const terminatePipelinesAll = () => Object.keys(pipelines).forEach(terminatePipelines);
+  const terminatePipelinesExcept = (type) => Object.keys(pipelines).filter(t => t !== type).forEach(terminatePipelines);
 
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       if (mutation.target === modalContainer) {
         const composePostModal = getComposePostModal(modalContainer);
         if (composePostModal !== null) {
-          for (const pipeline of composePostPipelines) {
-            pipeline.deploy(composePostModal, 'compose');
-          }
+          terminatePipelinesExcept('compose');
+          deployPipelines('compose', composePostModal);
         } else {
-          for (const pipeline of composePostPipelines) {
-            pipeline.terminate();
-          }
+          terminatePipelinesAll();
         }
       }
     });
--- src/utils/elementsFinder.js
+++ src/utils/elementsFinder.js
@@ -1,6 +1,11 @@
 export const LAST_TAB_INDEX = 0;
 
-const MODAL_CONTAINER_SELECTOR = '#root > div > div > div:nth-child(6)';
+const ROOT_CONTAINER_SELECTOR = '#root > div > div';
+const MODAL_CONTAINER_SELECTOR = 'div:nth-child(6)';
+
+export const getRootContainer = () => document.querySelector(ROOT_CONTAINER_SELECTOR);
+export const getModalContainer = (rootContainer) => rootContainer.querySelector(MODAL_CONTAINER_SELECTOR);
+
 const COMPOSE_POST_SELECTOR = '[data-testid="composePostView"]';
 const COMPOSE_CANCEL_BUTTON_SELECTOR = '[data-testid="composerCancelButton"]';
 const COMPOSE_POST_BUTTON_SELECTOR = '[data-testid="composerPublishBtn"]';
@@ -9,7 +14,6 @@ const LINK_BUTTON_SELECTOR = '[data-testid="addLinkCardBtn"]';
 const BUTTON_ROW_SELECTOR = 'div[tabindex] > div:nth-child(3)';
 const PHOTO_BUTTON_SELECTOR = `div[tabIndex='${LAST_TAB_INDEX}']`;
 
-export const getModalContainer = () => document.querySelector(MODAL_CONTAINER_SELECTOR);
 export const getComposePostModal = (modalContainer) => modalContainer.querySelector(COMPOSE_POST_SELECTOR);
 export const getExitButton = (modal) => modal.querySelector(COMPOSE_CANCEL_BUTTON_SELECTOR);
 export const getPostButton = (modal) => modal.querySelector(COMPOSE_POST_BUTTON_SELECTOR);
