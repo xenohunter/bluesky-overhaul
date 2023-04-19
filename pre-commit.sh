abf17bb90063ae1c0697025ae96fc138b33eb0ec
xenohunter <xenohunter@protonmail.ch>
2023-04-19 11:40:32 +0200

    Move TODO! checks to webpack

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# TODO : move this to the webpack pipeline
-# Check there is no TODO! in the code
-if grep -r "TODO!" ./src; then
-  echo "TODO! found in code"
-  exit 1
-fi
-
-# Build & make packages
-npm run build
+npm run pack
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
+const fs = require('fs');
+const glob = require('glob');
 const CopyPlugin = require('copy-webpack-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
-
 const {version} = require('./package.json');
 
+const SOURCE_ROOT = __dirname + '/src';
 const DIST_ROOT = __dirname + '/dist';
+
+const checkForTodo = () => {
+  const pattern = `${SOURCE_ROOT}/**/*.js`;
+  const files = glob.sync(pattern);
+
+  for (const file of files) {
+    const contents = fs.readFileSync(file, 'utf8');
+    const index = contents.indexOf('TODO!');
+    if (index !== -1) {
+      const line = contents.slice(0, index).split('\n').length;
+      throw new Error(`File ${file} contains a TODO! string at line ${line}`);
+    }
+  }
+
+  console.log('No TODO! strings found');
+};
 
 module.exports = (env) => {
   const browser = env['BROWSER'];
   if (!browser) {
     throw new Error('Please specify the target browser via the BROWSER environment variable');
   }
+
+  checkForTodo();
 
   const targetDir = `${DIST_ROOT}/${browser}`;
   const zipFilename = `bluesky-overhaul-${version}-${browser}.zip`;
